b781e56188849ef8a666f1ce0497c9d1098d87eb
JorgeAJX <76063633+JorgeAJX@users.noreply.github.com>
2021-07-25 00:57:21 -0500

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -98,7 +98,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Eunha',
+            'description': '',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -151,7 +151,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Eunha',
+            'description': '',
             'mimeType': mime_type,
         }
         if parent_id is not None:
